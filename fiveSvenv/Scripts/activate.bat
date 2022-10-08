85f4a6b6f3b133f40c1306361227f2b9fecdda81
kinge880 <brunomaya10@hotmail.com>
2022-10-08 12:07:29 -0500

    atualização

--- fiveSvenv/Scripts/activate.bat
+++ fiveSvenv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\Bruno\Desktop\5s Backend\fiveS\back-end\fiveSvenv
+set VIRTUAL_ENV=C:\Users\Bruno\Desktop\5s Backend\DJANGO5S\fiveSvenv
 
 if not defined PROMPT set PROMPT=$P$G
 
